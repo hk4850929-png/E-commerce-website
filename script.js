c72ddf07b6b1e027c811953cbeaf22e9870be209
hk4850929-png <hk4850929@gmail.com>
2026-07-22 11:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-// Mobile Menu Toggle
-const menu = document.querySelector(".menu");
-const nav = document.querySelector("nav");
+// Add to Cart Button
 
-menu.addEventListener("click", () => {
-    nav.classList.toggle("active");
+const buttons = document.querySelectorAll(".card button");
 
-    if (nav.classList.contains("active")) {
-        menu.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    } else {
-        menu.innerHTML = '<i class="fa-solid fa-bars"></i>';
-    }
+buttons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+alert("Product Added to Cart!");
+
 });
 
-// Close menu when a link is clicked
-document.querySelectorAll(".nav-links a").forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
-        menu.innerHTML = '<i class="fa-solid fa-bars"></i>';
-    });
-});
-
-// Simple Add to Cart Alert
-document.querySelectorAll(".product-card button").forEach(button => {
-    button.addEventListener("click", () => {
-        alert("✅ Product added to cart!");
-    });
 });
